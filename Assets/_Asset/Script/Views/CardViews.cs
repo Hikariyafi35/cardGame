950bc02c13951893ac3f2a92667d2d412330f6d7
Hikari Aufa Yafi <hikari32yafi@gmail.com>
2026-07-19 21:44:43 +0700

    make card view
    make prefabs cardview for container card later

---
+++ Assets/_Asset/Script/Views/CardViews.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.Rendering;
+public class CardViews : MonoBehaviour
+{
+    [SerializeField] private TMP_Text title;
+    [SerializeField] private TMP_Text description;
+    [SerializeField] private TMP_Text mana;
+    [SerializeField] private SpriteRenderer imageSR;
+    [SerializeField] private GameObject wrapper;
+}
